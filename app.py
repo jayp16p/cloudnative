b9e06536f6202c1ea27206932c6ef9f15b1b4753
jayp16p <106398902+jayp16p@users.noreply.github.com>
2023-07-23 14:40:18 -0400

    app.py and requirements added

---
+++ app.py
@@ -0,0 +1,16 @@
+import psutil
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    cpu_percent = psutil.cpu_percent()
+    mem_percent = psutil.virtual_memory().percent
+    Message = None
+    if cpu_percent > 80 or mem_percent > 80:
+        Message = "High CPU or memory utilization detected, please scale up."
+    return f"CPU utilization: {cpu_percent} and Memory Utilization: {mem_percent}"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
