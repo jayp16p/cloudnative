351dc7d2b7cebb87b59f3724c7bad8b494dc1884
jayp16p <106398902+jayp16p@users.noreply.github.com>
2023-07-23 14:58:50 -0400

    Added Dockerfile

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
     Message = None
     if cpu_percent > 80 or mem_percent > 80:
         Message = "High CPU or memory utilization detected, please scale up."
-    return render_template("index.html", cpu_metric=cpu_percent, mem_metric=mem_percent,Message=Message)
+    return render_template("index.html", cpu_metric=cpu_percent, mem_metric=mem_percent,message=Message)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
